39ff099ea2cfbe1798be20a8881833d7973cacde
pedroaurelio-n <69462126+pedroaurelio-n@users.noreply.github.com>
2025-01-05 19:30:45 -0300

    feat: artificial loading progress with fade in and out

--- Assets/_Game/Scripts/UI/LoadingScene.cs
+++ Assets/_Game/Scripts/UI/LoadingScene.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using System.Collections;
 using TMPro;
 using UnityEngine;
@@ -7,12 +8,18 @@ using UnityEngine.UI;
 public class LoadingScene : MonoBehaviour
 {
     const string PERCENTAGE_FORMAT = "{0}%";
-    
+    const float LOADING_SPEED = 0.1f;
+    const float FADE_DURATION = 0.8f;
+
+    [SerializeField] Canvas loadingCanvas;
+    [SerializeField] CanvasGroup fadeToBlackObject;
     [SerializeField] Button startButton;
     [SerializeField] GameObject[] loadingObjects;
     [SerializeField] TextMeshProUGUI loadingNumber;
+    [SerializeField] Image loadingFillBar;
 
-    int loadingProgress;
+    float loadingProgress;
+    float targetProgress;
 
     AsyncOperation mainSceneLoad;
     AsyncOperation loadingSceneUnload;
@@ -40,26 +47,67 @@ public class LoadingScene : MonoBehaviour
         foreach (GameObject obj in loadingObjects)
             obj.SetActive(true);
 
-        loadingNumber.text = string.Format(PERCENTAGE_FORMAT, loadingProgress);
+        UpdateLoadingUI();
 
         mainSceneLoad = SceneManager.LoadSceneAsync("MainScene", LoadSceneMode.Additive);
-        StartCoroutine(UpdateLoadingProgress());
+        StartCoroutine(UpdateLoadingProgress(mainSceneLoad, null));
     }
 
     void CompleteLoad ()
     {
-        loadingSceneUnload = SceneManager.UnloadSceneAsync("LoadingScene");
+        loadingCanvas.gameObject.SetActive(false);
+        FadeOut();
     }
 
-    IEnumerator UpdateLoadingProgress ()
+    IEnumerator UpdateLoadingProgress (AsyncOperation loadOperation, AsyncOperation unloadOperation)
     {
-        while (!mainSceneLoad.isDone)
+        loadOperation.allowSceneActivation = false;
+        
+        while (loadingProgress < 100)
         {
-            loadingProgress = Mathf.RoundToInt(mainSceneLoad.progress * 100);
-            loadingNumber.text = string.Format(PERCENTAGE_FORMAT, loadingProgress);
+            float loadProgress = loadOperation.progress < 0.9f ? loadOperation.progress : 1f;
+            float unloadProgress = unloadOperation?.progress ?? 1f;
+            float unloadWeight = unloadOperation != null ? 0.5f : 0f;
+            float loadWeight = 1f - unloadWeight;
+            
+            targetProgress = Mathf.RoundToInt((loadProgress * loadWeight + unloadProgress * unloadWeight) * 100);
+
+            while (loadingProgress < targetProgress && targetProgress != 0)
+            {
+                loadingProgress += LOADING_SPEED;
+                UpdateLoadingUI();
+                yield return null;
+            }
+
+            UpdateLoadingUI();
             yield return null;
         }
+        
+        loadingProgress = 100;
+        UpdateLoadingUI();
 
+        FadeIn().OnComplete(() => loadOperation.allowSceneActivation = true);
+
+        while (!loadOperation.isDone)
+            yield return null;
+        
         CompleteLoad();
+    }
+
+    void UpdateLoadingUI ()
+    {
+        loadingNumber.text = string.Format(PERCENTAGE_FORMAT, Mathf.RoundToInt(loadingProgress));
+        loadingFillBar.fillAmount = loadingProgress / 100;
+    }
+    
+    //TODO pedro: isolate fade to it's own class
+    Tween FadeIn ()
+    {
+        return DOTween.To(() => fadeToBlackObject.alpha, x => fadeToBlackObject.alpha = x, 1, FADE_DURATION);
+    }
+    
+    Tween FadeOut ()
+    {
+        return DOTween.To(() => fadeToBlackObject.alpha, x => fadeToBlackObject.alpha = x, 0, FADE_DURATION);
     }
 }
